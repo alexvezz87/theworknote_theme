001980ddfb61bdcb8d1dfa4a9d1de439b060da76
Alex <Alex@Alex-Vaio>
2016-03-30 19:16:02 +0200

    adattamenti

--- invia-cv.php
+++ invia-cv.php
@@ -10,13 +10,55 @@
 
 get_header();
 
+$type = 0;
+
+$url = get_template_directory_uri ().'/images/desktop/';
+$class = "";
+
+if(strtolower(get_the_title())== 'scopri cv'){
+    $type = 1; //Offrire lavoro 
+    $url .= 'sfondo-scopri-cv.jpg';
+    $class = 'scopri-cv';
+}
+else if(strtolower(get_the_title())== 'inserisci cv'){
+    $type = 2; //Richiedere lavoro
+    $url .= 'sfondo-invia-cv.jpg';
+}
+
 ?>
 
-<div class="main-container invia-cv">
-    <div class="first-main-container">
+<div class="main-container invia-cv <?php echo $class ?>">
+    
+    <div class="first-main-container visible-xs">
         <div class="container-1024">
             <h1 class="col-xs-12">
-                The WorkNote CV<br>richiedere lavoro
+                <?php
+                    if($type == 1){
+                        echo 'The Work Note CV<br>offrire lavoro';
+                    }
+                    else if($type == 2){
+                        echo 'The WorkNote CV<br>richiedere lavoro';
+                    }
+                ?>
+                
+            </h1>
+            <div class="clear"></div>
+        </div>        
+    </div>
+         
+    
+    <div class="first-main-container hidden-xs" style="background:url('<?php echo $url ?>')">
+        <div class="container-1024">
+            <h1 class="col-xs-12">
+                <?php
+                    if($type == 1){
+                        echo 'The Work Note CV<br>offrire lavoro';
+                    }
+                    else if($type == 2){
+                        echo 'The WorkNote CV<br>richiedere lavoro';
+                    }
+                ?>
+                
             </h1>
             <div class="clear"></div>
         </div>        
